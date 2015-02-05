802a38f40fcb3e69647ae21b60c20ea0d76dd232
robkemp <rjameskemp@gmail.com>
2015-02-05 11:19:08 -0700

    income category collapse and typo fix

--- R/ms_income.R
+++ R/ms_income.R
@@ -14,6 +14,7 @@ require(car, quietly=TRUE)
 require(stringr, quietly=TRUE)
 require(grid, quietly=TRUE)
 require(scales, quietly=TRUE)
+require(tidyr, quietly=TRUE)
 require(dplyr, quietly=TRUE)
 require(codemog, quietly=TRUE)
 
@@ -24,13 +25,17 @@ hhinc1=codemog_api(data="b19001",db="acs0913", geonum=paste("1", state, fips, se
   mutate(geoname=str_trim(geoname, side="both"),
          var2=str_sub(var, -2,-1),
          var3=as.numeric(as.character(var2)),
-         cat=ordered(var3, levels=2:17, labels=c("Less than $10,000","$10,000 to $14,999","$15,000 to $19,999",
-                                                 "$20,000 to $24,999", "$25,000 to $29,999", "$30,000 to $34,999",
-                                                 "$35,000 to $39,999", "$40,000 to $44,999", "$45,000 to $49,999",
-                                                 "$50,000 to $59,999","$60,000 to $74,999","$75,000 to $99,999","$100,000 to $124,999",
-                                                 "$125,000 to $149,999","$150,000 to $199,999","$200,000 or more")))%>%
+         group=recode(var3, "2=1; 3:4=2; 5:6=3;7:8=4;9:10=5; 11=6;12=7;13=8;14=9;
+                      15=10;16=11;17=12"))%>%
+  group_by(geoname,group)%>%
+  summarise(value=sum(as.numeric(value)))%>%
+
+  mutate(cat=ordered(group, levels=1:12, labels=c("Less than $10,000","$10,000 to $19,999","$20,000 to $29,999",
+                                                  "$30,000 to $39,999", "$40,000 to $49,999", "$50,000 to $59,999",
+                                                  "$60,000 to $74,999","$75,000 to $99,999","$100,000 to $124,999",
+                                                  "$125,000 to $149,999","$150,000 to $199,999","$200,000 or more")))%>%
   separate(geoname, into=c("geoname", "state_name"), sep=",")%>%
-  select(-state_name, -var2,-var3)%>%
+  select(-state_name, -group)%>%
   mutate(p=as.numeric(value)/sum(as.numeric(value)))
 
 hhinc2=codemog_api(data="b19001",db="acs0913", geonum=paste("1", state2, fips2, sep=""), meta="no")%>%
@@ -39,12 +44,15 @@ hhinc2=codemog_api(data="b19001",db="acs0913", geonum=paste("1", state2, fips2, 
   mutate(geoname=str_trim(geoname, side="both"),
          var2=str_sub(var, -2,-1),
          var3=as.numeric(as.character(var2)),
-         cat=ordered(var3, levels=2:17, labels=c("Less than $10,000","$10,000 to $14,999","$15,000 to $19,999",
-                                                 "$20,000 to $24,999", "$25,000 to $29,999", "$30,000 to $34,999",
-                                                 "$35,000 to $39,999", "$40,000 to $44,999", "$45,000 to $49,999",
-                                                 "$50,000 to $59,999","$60,000 to $74,999","$75,000 to $99,999","$100,000 to $124,999",
-                                                 "$125,000 to $149,999","$150,000 to $199,999","$200,000 or more")))%>%
-  select( -var2,-var3)%>%
+         group=recode(var3, "2=1; 3:4=2; 5:6=3;7:8=4;9:10=5; 11=6;12=7;13=8;14=9;
+                      15=10;16=11;17=12"))%>%
+  group_by(geoname,group)%>%
+  summarise(value=sum(as.numeric(value)))%>%
+
+  mutate(cat=ordered(group, levels=1:12, labels=c("Less than $10,000","$10,000 to $19,999","$20,000 to $29,999",
+                                                  "$30,000 to $39,999", "$40,000 to $49,999", "$50,000 to $59,999",
+                                                  "$60,000 to $74,999","$75,000 to $99,999","$100,000 to $124,999",
+                                                  "$125,000 to $149,999","$150,000 to $199,999","$200,000 or more")))%>%
   mutate(p=as.numeric(value)/sum(as.numeric(value)))
 
 hhinc=bind_rows(hhinc1, hhinc2)
@@ -57,7 +65,6 @@ p=hhinc%>%ggplot(aes(x=cat, y=as.numeric(p), fill=geoname))+
   theme_codemog(base_size=base)+
   theme(axis.text.x=element_text(angle=45, hjust=1))+
   labs(x="Income (in 2013 Dollars)", y="Percentage", title="Household Income Distribution\n Source: 2013 ACS 5-Year File")
-p
 
 return(p)
 }
