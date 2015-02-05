fcedc4717d4d08f9b3dff00c275a98d37b607f0a
robkemp <rjameskemp@gmail.com>
2015-02-05 14:45:26 -0700

    added percents to race and housing

--- R/ms_housing.R
+++ R/ms_housing.R
@@ -14,43 +14,65 @@ require(dplyr, quietly=TRUE)
 
 h3_10=codemog_api(data="h3", geonum=paste("1", state, fips, sep=""),meta="no")
 h3_10[,7:ncol(h3_10)]=as.numeric(as.character(h3_10[,7:ncol(h3_10)]))
-h3_10=h3_10%>%rename(Total=h3001, Occupied=h3002, Vacant=h3003)
+h3_10=h3_10%>%rename(Total=h3001, Occupied=h3002, Vacant=h3003)%>%
+  mutate(OccPercent=percent(Occupied/Total),
+         VacPercent=percent(Vacant/Total),
+         Occupied=comma(Occupied),
+         Vacant=comma(Vacant))
 
 h4_10=codemog_api(data="h4", geonum=paste("1", state, fips, sep=""),meta="no")
 h4_10[,7:ncol(h4_10)]=as.numeric(as.character(h4_10[,7:ncol(h4_10)]))
 h4_10=h4_10%>%  mutate(Owner=h4002+h4003,
                        Renter=h4004)%>%
-  select(-h4001:-h4004)
+  select(-h4001:-h4004)%>%
+  mutate(ownPercent=percent(Owner/(Owner+Renter)),
+         rentPercent=percent(Renter/(Owner+Renter)),
+         Owner=comma(Owner),
+         Renter=comma(Renter))
 h5_10=codemog_api(data="h5", geonum=paste("1", state, fips, sep=""),meta="no")
 h5_10[,7:ncol(h5_10)]=as.numeric(as.character(h5_10[,7:ncol(h5_10)]))
 h5_10=h5_10%>%
   mutate(Seasonal=h5006,
          Other=h5002+h5003+h5004+h5005+h5007+h5008)%>%
-  select(-h5001:-h5008)
+  select(-h5001:-h5008)%>%
+  mutate(seasPercent=percent(Seasonal/(Other+Seasonal)),
+         otherPercent=percent(Other/(Other+Seasonal)),
+         Other=comma(Other),
+         Seasonal=comma(Seasonal))
 housing_10=inner_join(h3_10,h5_10)%>%inner_join(h4_10)%>%
-  gather(var, Census.2010, Total:Renter, -geoname, -state, -county, -place,-tract,-bg,-geonum)
+  gather(var, Census.2010, Total:rentPercent, -geoname, -state, -county, -place,-tract,-bg,-geonum)
 h3_00=codemog_api(data="h3",db="c2000", geonum=paste("1", state, fips, sep=""),meta="no")
 h3_00[,7:ncol(h3_00)]=as.numeric(as.character(h3_00[,7:ncol(h3_00)]))
-h3_00=h3_00%>%rename(Total=h3001, Occupied=h3002, Vacant=h3003)
+h3_00=h3_00%>%rename(Total=h3001, Occupied=h3002, Vacant=h3003)%>%
+  mutate(OccPercent=percent(Occupied/Total),
+         VacPercent=percent(Vacant/Total),
+         Total=comma(Total),
+         Occupied=comma(Occupied),
+         Vacant=comma(Vacant))
 
 h4_00=codemog_api(data="h4",db="c2000", geonum=paste("1", state, fips, sep=""),meta="no")
 h4_00[,7:ncol(h4_00)]=as.numeric(as.character(h4_00[,7:ncol(h4_00)]))
 h4_00=h4_00%>%  rename(Owner=h4002,
                        Renter=h4003)%>%
-  select(-h4001)
+  select(-h4001)%>%
+  mutate(ownPercent=percent(Owner/(Owner+Renter)),
+         rentPercent=percent(Renter/(Owner+Renter)),
+         Owner=comma(Owner),
+         Renter=comma(Renter))
 h5_00=codemog_api(data="h5",db="c2000", geonum=paste("1", state, fips, sep=""),meta="no")
 h5_00[,7:ncol(h5_00)]=as.numeric(as.character(h5_00[,7:ncol(h5_00)]))
 h5_00=h5_00%>%
   mutate(Seasonal=h5005,
          Other=h5002+h5003+h5004+h5006+h5007)%>%
-  select(-h5001:-h5007)
+  select(-h5001:-h5007)%>%
+  mutate(seasPercent=percent(Seasonal/(Other+Seasonal)),
+         otherPercent=percent(Other/(Other+Seasonal)),
+         Other=comma(Other),
+         Seasonal=comma(Seasonal))
+
 housing_00=inner_join(h3_00,h5_00)%>%inner_join(h4_00)%>%
-  gather(var, Census.2000, Total:Renter, -geoname, -state, -county, -place,-tract,-bg,-geonum)
+  gather(var, Census.2000, Total:rentPercent, -geoname, -state, -county, -place,-tract,-bg,-geonum)
 
-housing=inner_join(housing_00,housing_10)%>%
-  mutate(Change=percent(((Census.2010-Census.2000)/Census.2000)),
-         Census.2000=comma(Census.2000),
-         Census.2010=comma(Census.2010))
-
+housing=inner_join(housing_00,housing_10)
 return(housing)
 }
--- R/ms_race.R
+++ R/ms_race.R
@@ -13,20 +13,46 @@ ms_race=function(fips, state="08"){
   require(scales, quietly=TRUE)
   require(dplyr, quietly=TRUE)
 
-  p11_10=codemog_api(data="p11", geonum=paste("1", state, fips, sep=""),meta="no")%>%
-    select(geoname:p11011)%>%
-    rename(TotalPop=p11001, Hispanic=p11002, NonHispanic=p11003, NHWhite=p11005, NHBlack=p11006,
-           NHAIAN=p11007, NHAsian=p11008, NHNHOPI=p11009, NHOther=p11010, NHTwo=p11011 )%>%
-    gather(race, Census.2010, TotalPop:NHTwo, -geoname:-geonum)
+  p11_10=codemog_api(data="p11", geonum=paste("1", state, fips, sep=""),meta="no")
+  p11_10[,7:ncol(p11_10)]=as.numeric(as.character(p11_10[,7:ncol(p11_10)]))
 
-  p4_00=codemog_api(data="p4", db="c2000",geonum=paste("1", state, fips, sep=""),meta="no")%>%
+  p11_10=p11_10%>%
+    select(geoname:p11011)%>%
+    mutate(TotalPop=p11001, Hispanic=p11002, NonHispanic=p11003, NHWhite=p11005, NHBlack=p11006,
+           NHAIAN=p11007, NHAsian=p11008, NHNHOPI=p11009, NHOther=p11010, NHTwo=p11011,
+           HispanicP=percent(Hispanic/TotalPop),
+           NonHispanicP=percent(NonHispanic/TotalPop),
+           NHWhiteP=percent(NHWhite/NonHispanic),
+           NHBlackP=percent(NHBlack/NonHispanic),
+           NHAIANP=percent(NHAIAN/NonHispanic),
+           NHAsianP=percent(NHAsian/NonHispanic),
+           NHNHOPIP=percent(NHNHOPI/NonHispanic),
+           NHOtherP=percent(NHOther/NonHispanic),
+           NHTwoP=percent(NHTwo/NonHispanic),
+           TotalPop=comma(p11001), Hispanic=comma(p11002), NonHispanic=comma(p11003), NHWhite=comma(p11005), NHBlack=comma(p11006),
+           NHAIAN=comma(p11007), NHAsian=comma(p11008), NHNHOPI=comma(p11009), NHOther=comma(p11010), NHTwo=comma(p11011))%>%
+    select(-p11001:-p11011)%>%
+    gather(race, Census.2010, TotalPop:NHTwoP, -geoname:-geonum)
+
+  p4_00=codemog_api(data="p4", db="c2000",geonum=paste("1", state, fips, sep=""),meta="no")
+  p4_00[,7:ncol(p4_00)]=as.numeric(as.character(p4_00[,7:ncol(p4_00)]))
+  p4_00=p4_00%>%
     select(geoname:p4011)%>%
-    rename(TotalPop=p4001, Hispanic=p4002, NonHispanic=p4003, NHWhite=p4005, NHBlack=p4006,
-           NHAIAN=p4007, NHAsian=p4008, NHNHOPI=p4009, NHOther=p4010, NHTwo=p4011 )%>%
-    gather(race, Census.2000, TotalPop:NHTwo, -geoname:-geonum)
-p4=inner_join(p11_10, p4_00)%>%
-  mutate(Change=percent((as.numeric(Census.2010)-as.numeric(Census.2000))/as.numeric(Census.2000)),
-         Census.2000=comma(as.numeric(Census.2000)),
-         Census.2010=comma(as.numeric(Census.2010)))
+    mutate(TotalPop=p4001, Hispanic=p4002, NonHispanic=p4003, NHWhite=p4005, NHBlack=p4006,
+           NHAIAN=p4007, NHAsian=p4008, NHNHOPI=p4009, NHOther=p4010, NHTwo=p4011,
+           HispanicP=percent(Hispanic/TotalPop),
+           NonHispanicP=percent(NonHispanic/TotalPop),
+           NHWhiteP=percent(NHWhite/NonHispanic),
+           NHBlackP=percent(NHBlack/NonHispanic),
+           NHAIANP=percent(NHAIAN/NonHispanic),
+           NHAsianP=percent(NHAsian/NonHispanic),
+           NHNHOPIP=percent(NHNHOPI/NonHispanic),
+           NHOtherP=percent(NHOther/NonHispanic),
+           NHTwoP=percent(NHTwo/NonHispanic),
+           TotalPop=comma(p4001), Hispanic=comma(p4002), NonHispanic=comma(p4003), NHWhite=comma(p4005), NHBlack=comma(p4006),
+           NHAIAN=comma(p4007), NHAsian=comma(p4008), NHNHOPI=comma(p4009), NHOther=comma(p4010), NHTwo=comma(p4011))%>%
+    select(-p4001:-p4011)%>%
+    gather(race, Census.2000, TotalPop:NHTwoP, -geoname:-geonum)
+p4=inner_join(p11_10, p4_00)
 return(p4)
 }
