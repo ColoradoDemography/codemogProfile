693c3bf93cdf73f706707c8edb4433a9467086bf
robkemp <rjameskemp@gmail.com>
2015-03-09 11:28:44 -0600

    added ms_ed and ms_census_age functions from codemog to codemogProfile package to harmonizes the purposes of each package

---
+++ R/ms_census_age.R
@@ -0,0 +1,70 @@
+#' Creates a chart comparing an areas age distributions in 2000 and 2010
+#'
+#' Uses the codemog_api function to access Census data to create a ggplot2 chart for
+#' use in profiles comparing the age distribution at the 2000 and 2010 Censuses.  Included here as more of an example.
+#'
+#' @param fips is the fips code for the place or county
+#' @param state is the state that of the fips in the first argument
+
+ms_census_age=function(fips, state="08", base=12){
+  require(ggplot2, quietly=TRUE)
+  require(stringi, quietly=TRUE)
+  require(scales, quietly=TRUE)
+  require(grid, quietly=TRUE)
+  require(reshape2, quietly=TRUE)
+  require(tidyr, quietly=TRUE)
+  require(dplyr, quietly=TRUE)
+  d10=codemog_api(data="p12",db="c2010",geonum=paste("1",state , fips,sep=""),meta="no")
+  d00=codemog_api(data="p12",db="c2000",geonum=paste("1",state , fips,sep=""), meta="no")
+  d10[,7:56]=as.numeric(as.character(d10[,7:56]))
+  d00[,7:56]=as.numeric(as.character(d00[,7:56]))
+  d10c=d10%>%
+    mutate(age1=p12003+p12027+p12004+p12028,
+           age2=p12005+p12029+p12006+p12030,
+           age3=p12007+p12031+p12008+p12009+p12010+p12032+p12033+p12034,
+           age4=p12011+p12012+p12035+p12036,
+           age5=p12013+p12014+p12037+p12038,
+           age6=p12015+p12016+p12039+p12040,
+           age7=p12017+p12018+p12019+p12041+p12042+p12043,
+           age8=p12020+p12021+p12022+p12023+p12024+p12025+
+             p12044+p12045+p12046+p12047+p12048+p12049)%>%
+    select(geoname:geonum,age1:age8)%>%
+    melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
+    mutate(
+      agecat=ordered(as.factor(variable), levels=c("age1", "age2", "age3", "age4",
+                                                   "age5", "age6", "age7", "age8"),
+                     labels=c("Less than 9", "10 to 17", "18 to 24", "25 to 34","35 to 44",
+                              "45 to 54", "55 to 64", "65 and Over")),
+      year="2010",
+      geoname=stri_trans_general(geoname, id="Title"))
+  d00c=d00%>%
+    mutate(age1=p12003+p12027+p12004+p12028,
+           age2=p12005+p12029+p12006+p12030,
+           age3=p12007+p12031+p12008+p12009+p12010+p12032+p12033+p12034,
+           age4=p12011+p12012+p12035+p12036,
+           age5=p12013+p12014+p12037+p12038,
+           age6=p12015+p12016+p12039+p12040,
+           age7=p12017+p12018+p12019+p12041+p12042+p12043,
+           age8=p12020+p12021+p12022+p12023+p12024+p12025+
+             p12044+p12045+p12046+p12047+p12048+p12049)%>%
+    select(geoname:geonum,age1:age8)%>%
+    melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
+    mutate(
+      agecat=ordered(as.factor(variable), levels=c("age1", "age2", "age3", "age4",
+                                                   "age5", "age6", "age7", "age8"),
+                     labels=c("Less than 9", "10 to 17", "18 to 24", "25 to 34","35 to 44",
+                              "45 to 54", "55 to 64", "65 and Over")),
+      year="2000",
+      geoname=stri_trans_general(geoname, id="Title"))
+  d=rbind(d10c, d00c)#%>%spread(year,value)
+  p=ggplot(d, aes(x=agecat, y=value, fill=year))+
+    geom_bar(stat="identity", position="dodge")+#, fill=rgb(31,74,126, max=255))+
+    scale_y_continuous(label=comma)+
+    scale_fill_manual(values=c(rgb(31,74,126, max=255), rgb(192,80,77,max=255)),
+                      name="Census Year",
+                      breaks=c("2000", "2010"),
+                      labels=c("2000","2010"))+
+    theme_codemog(base_size=base)+
+    labs(x="Age", y="Population", title=paste(d10c$geoname, "Population by Age \nSource: U.S. Census Bureau"))
+  return(p)
+}
---
+++ R/ms_ed.R
@@ -0,0 +1,71 @@
+#' Creates a Chart comparing educational attainment of two areas
+#'
+#' Uses the codemog_api function to access ACS data (defaults to 13-5yr) to create a ggplot2 chart for
+#' use in profiles.  Included here as more of an example.
+#'
+#' @param fips is the fips code for the main area to be compared
+#' @param state is the state that the original fips
+#' @param fips2 is the second area to be compared Defaults to Blank
+#' @param state2 is the state of the second place to be compared, is set to call up CO since fips2 is blank Defaults to 08
+#' @param base is the abse text size for the ggplot2 object and codemog_theme()
+ms_ed=function(fips, state="08", fips2="", state2="08", base=12){
+  require(stringi, quietly=TRUE)
+  require(ggplot2, quietly=TRUE)
+  require(scales, quietly=TRUE)
+  require(grid, quietly=TRUE)
+  require(reshape2, quietly=TRUE)
+  require(tidyr, quietly=TRUE)
+  require(dplyr, quietly=TRUE)
+
+
+  d13p=codemog_api(data="b15003",db="acs0913",geonum=paste("1",state , fips,sep=""),meta="no")
+  d13p[,7:32]=as.numeric(as.character(d13p[,7:32]))
+  d13pm=d13p%>%
+    mutate(ed1=b15003002+b15003003+b15003004+b15003005+b15003006+b15003007+b15003008+b15003009+b15003010+b15003011+
+             b15003012+b15003013+b15003014+b15003015+b15003016,
+           ed2=b15003017+b15003018,
+           ed3=b15003019+b15003020+b15003021,
+           ed4=b15003022,
+           ed5=b15003023+b15003024+b15003025)%>%
+    select(geoname:geonum,ed1:ed5)%>%
+    melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
+    mutate(agecat=ordered(as.factor(variable), levels=c("ed1", "ed2", "ed3", "ed4",
+                                                        "ed5"),
+                          labels=c("Less than High School",
+                                   "High School Graduate \n(or GED)","Some College or \nAssociate's Degree", "Bachelor's Degree",
+                                   "Graduate or \nProfessional Degree")))%>%
+    separate(geoname, into=c("geoname","statename"),sep=",")%>%
+    select(-statename)%>%
+    mutate(geoname=stri_trans_general(geoname,id="Title"))
+
+  d13c=codemog_api(data="b15003",db="acs0913",geonum=paste("1",state2 , fips2,sep=""),meta="no")
+  d13c[,7:32]=as.numeric(as.character(d13c[,7:32]))
+  d13cm=d13c%>%
+    mutate(ed1=b15003002+b15003003+b15003004+b15003005+b15003006+b15003007+b15003008+b15003009+b15003010+b15003011+
+             b15003012+b15003013+b15003014+b15003015+b15003016,
+           ed2=b15003017+b15003018,
+           ed3=b15003019+b15003020+b15003021,
+           ed4=b15003022,
+           ed5=b15003023+b15003024+b15003025)%>%
+    select(geoname:geonum,ed1:ed5)%>%
+    melt(id=c("geoname", "state", "county", "place", "tract", "bg", "geonum"))%>%
+    mutate(agecat=ordered(as.factor(variable), levels=c("ed1", "ed2", "ed3", "ed4",
+                                                        "ed5"),
+                          labels=c("Less than High School",
+                                   "High School Graduate \n(or GED)","Some College or \nAssociate's Degree", "Bachelor's Degree",
+                                   "Graduate or \nProfessional Degree")))%>%
+    mutate(geoname=stri_replace_all_charclass(geoname, "\\p{WHITE_SPACE}", ""))
+  d=rbind(d13cm,d13pm)%>%
+    group_by(geoname)%>%
+    mutate(p=value/sum(value))
+  p=ggplot(d, aes(x=agecat, y=p, fill=geoname))+
+    geom_bar(stat="identity", position="dodge")+#, fill=rgb(31,74,126, max=255))+
+    scale_y_continuous(label=percent)+
+    scale_fill_manual(values=c(rgb(31,74,126, max=255), rgb(192,80,77,max=255)),
+                      name="Geography")+
+    theme_codemog(base_size=base)+
+    theme(axis.text.x=element_text(angle=0))+
+    labs(x="Educational Attainment", y="Population", title="Educational Attainment for 25 and Older \nSource: ACS 2013 5-Year File")
+  return(p)
+
+}
