8a057fefa0afb348982c605c5a3fc259465bb4d6
robkemp <rjameskemp@gmail.com>
2015-05-28 07:39:51 -0600

    added documentation and some small fixes to the formatting of the pop trend chart

--- R/cp_poptrend.R
+++ R/cp_poptrend.R
@@ -1,8 +1,15 @@
-fips="089"
-countyname="Otero"
+#' Creates a chart with two plots, one for population trend, one for components of change
+#'
+#' This function creates two seperate time-series plots, a line chart for popultion trends
+#' and a dodged bar chart for components of change over time.  The two plots are stacked together and
+#' placed into one chart.
+#'
+#' @param fips is the fips code for the main area to be compared
+#' @param state is the state that the original fips
+#' @param base is the base text size for the ggplot2 object and codemog_theme()
 
 
-cp_poptrend=function(fips,countyname){
+cp_poptrend=function(fips,countyname, base=12){
 require(codemog, quietly = TRUE)
 require(ggplot2, quietly = TRUE)
 require(gridExtra, quietly = TRUE)
@@ -21,7 +28,7 @@ fips=as.numeric(fips)
                     name="",
                     breaks=c("netMigration", "naturalIncrease"),
                     labels=c("Net Migration","Natural Increase"))+
-  theme_codemog()+
+  theme_codemog(base_size=base)+
   labs(y="Component Value", x= "Year")
 
 pd2=county_profile%>%
@@ -31,6 +38,15 @@ pd2=county_profile%>%
   scale_x_continuous(breaks=1985:2013)+
   theme_codemog()+
   labs(y="Population", x="", title=paste(countyname, "County Population Trend and Components of Change, 1985 to 2013\nSource: State Demography Office"))
+
+# pd=ggplot_gtable(ggplot_build(pd))
+# pd2=ggplot_gtable(ggplot_build(pd2))
+#
+# maxWidth = unit.pmax(pd$widths[2:3], pd2$widths[2:3])
+#
+# pd$widths[2:3] <- maxWidth
+# pd2$widths[2:3] <- maxWidth
+#
 pd3=grid.arrange(pd2,pd)
 
 return(pd3)
