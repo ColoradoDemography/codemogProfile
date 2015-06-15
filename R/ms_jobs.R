b4bb140b8119f637dba7e4546bb90dfb50f9791b
robkemp <rjameskemp@gmail.com>
2015-06-15 13:48:14 -0600

    patched ms_jobs year variable typing issue

--- R/ms_jobs.R
+++ R/ms_jobs.R
@@ -19,7 +19,7 @@ ms_jobs=function(fips, countyname, base=12){
     filter(countyfips==as.numeric(fips),sector_id==0)%>%
     mutate(jobs=recode(jobs, "'S'=NA"),
            jobs=round(as.numeric(jobs),0))%>%
-    ggplot(aes(x=year, y=as/numeric(jobs), group=countyfips))+
+    ggplot(aes(x=year, y=as.numeric(jobs), group=countyfips))+
     geom_rect(aes(xmin=2008, xmax=2010, ymin=-Inf, ymax=+Inf), fill=rgb(208, 210, 211, max = 255), alpha=.03)+
     geom_rect(aes(xmin=2001, xmax=2003, ymin=-Inf, ymax=+Inf), fill=rgb(208, 210, 211, max = 255), alpha=.03)+
     geom_line(color=rgb(0, 168, 58, max = 255), size=1.5)+
