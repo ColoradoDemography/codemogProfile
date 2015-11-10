b930646d50c184d34c8497493f376d19bf80444b
robkemp <rjameskemp@gmail.com>
2015-11-10 14:23:20 -0700

    updated years in jobs/mig and pop trend charts

--- R/cp_migjobs.R
+++ R/cp_migjobs.R
@@ -10,35 +10,36 @@
 #'
 #'
 cp_migjobs=function(fips, countyname, base=12){
-require(codemog, quietly = TRUE)
-require(ggplot2, quietly = TRUE)
-require(grid, quietly = TRUE)
-require(dplyr, quietly = TRUE)
-require(tidyr, quietly = TRUE)
+  require(codemog, quietly = TRUE)
+  require(ggplot2, quietly = TRUE)
+  require(grid, quietly = TRUE)
+  require(dplyr, quietly = TRUE)
+  require(tidyr, quietly = TRUE)
 
-j=county_jobs%>%
-  filter(sector_id==0, year<=2013)%>%
-  select(countyfips, year, jobs)%>%
-  group_by(countyfips)%>%
-  arrange(year)%>%
-  mutate(jobs=as.numeric(jobs),
-         jobChange=jobs-lag(jobs))
-nm=county_profile%>%
-  filter(year>2000)%>%
-  select(year, countyfips, netMigration)
+  j=county_jobs%>%
+    filter(sector_id==0, year<=2014)%>%
+    select(countyfips, year, jobs)%>%
+    group_by(countyfips)%>%
+    arrange(year)%>%
+    mutate(jobs=as.numeric(jobs),
+           jobChange=jobs-lag(jobs))
+  nm=county_profile%>%
+    filter(year>2000)%>%
+    select(year, countyfips, netMigration)
 
-p=inner_join(j,nm, by=c("countyfips", "year"))%>%
-filter(countyfips==as.numeric(fips), year>2001)%>%
-  ggplot()+
-  geom_bar(stat="identity",aes(x=year, y=jobChange, fill="jobChange"))+
-  geom_line(aes(x=year, y=netMigration,color="netMigration"), size=1.15)+
-  scale_x_continuous(breaks=2002:2013)+
-  scale_color_manual(" ",values=c("jobChange"=rgb(31,74,126, max=255), "netMigration"=rgb(191,32,38, max=255)),
-                     labels=c("Net Migration"))+
-  scale_fill_manual("", values=rgb(31,74,126, max=255), labels="Job Change")+
-  theme_codemog(base_size=base)+
-  theme(legend.box="horizontal")+
-  labs(x="Year", y="Count", title=paste(countyname, "County Job Change and Net Migration, 2002 to 2013\nSource: State Demography Office"))
+  p=inner_join(j,nm, by=c("countyfips", "year"))%>%
+    filter(countyfips==as.numeric(fips), year>2001)%>%
+    ggplot()+
+    geom_bar(stat="identity",aes(x=year, y=jobChange, fill="jobChange"))+
+    geom_line(aes(x=year, y=netMigration,color="netMigration"), size=1.15)+
+    scale_x_continuous(breaks=2002:2014)+
+    scale_color_manual(" ",values=c("jobChange"=rgb(31,74,126, max=255), "netMigration"=rgb(191,32,38, max=255)),
+                       labels=c("Net Migration"))+
+    scale_fill_manual("", values=rgb(31,74,126, max=255), labels="Job Change")+
+    theme_codemog(base_size=base)+
+    theme(legend.box="horizontal",
+          plot.title=element_text(size=rel(1.25)))+
+    labs(x="Year", y="Count", title=paste(countyname, "County Job Change and Net Migration, 2002 to 2014\nSource: State Demography Office"))
 
-return(p)
+  return(p)
 }
--- R/cp_poptrend.R
+++ R/cp_poptrend.R
@@ -24,32 +24,27 @@ fips=as.numeric(fips)
   filter(variable=="netMigration" | variable=="naturalIncrease", countyfips==fips)%>%
   ggplot(aes(x=year, y=value, fill=variable))+
   geom_bar(stat="identity",position="dodge")+
-  scale_x_continuous(breaks=1985:2013)+
+  scale_x_continuous(breaks=1985:max(unique(county_profile$year)))+
   scale_y_continuous(labels=comma)+
   scale_fill_manual(values=c(rgb(31,74,126, max=255), rgb(192,32,38,max=255)),
                     name="",
                     breaks=c("netMigration", "naturalIncrease"),
                     labels=c("Net Migration","Natural Increase"))+
   theme_codemog(base_size=base)+
+  theme(axis.text.x=element_text(angle=90))+
   labs(y="Component Value", x= "Year")
 
 pd2=county_profile%>%
   filter(countyfips==fips)%>%
   ggplot()+
   geom_line(aes(x=year, y=(householdPopulation+groupQuartersPopulation)), color=rgb(0,149,58, max=255), size=1.15)+
-  scale_x_continuous(breaks=1985:2013)+
+  scale_x_continuous(breaks=1985:max(unique(county_profile$year)))+
   scale_y_continuous(labels=comma)+
   theme_codemog(base_size=base)+
-  labs(y="Population", x="", title=paste(countyname, "County Population Trend and Components of Change, 1985 to 2013\nSource: State Demography Office"))
+  theme(axis.text.x=element_text(angle=90),
+        plot.title=element_text(size=rel(1.25)))+
+  labs(y="Population", x="", title=paste(countyname, "County Population Trend and Components of Change\nSource: State Demography Office"))
 
-# pd=ggplot_gtable(ggplot_build(pd))
-# pd2=ggplot_gtable(ggplot_build(pd2))
-#
-# maxWidth = unit.pmax(pd$widths[2:3], pd2$widths[2:3])
-#
-# pd$widths[2:3] <- maxWidth
-# pd2$widths[2:3] <- maxWidth
-#
 pd3=grid.arrange(pd2,pd)
 
 return(pd3)
